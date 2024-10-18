1c46a30566499ca2d2666048419950d610e9494b
Roi Gamba <roi.gamba@trustarc.com>
2024-10-18 16:43:44 +0800

    improve table ui and copy table functionality

--- app/public/index.php
+++ app/public/index.php
@@ -11,49 +11,35 @@ $enableFetching = false;
 $key = '';
 $secret = '';
 $token = '';
+$accountId = '';
+
+$jsonFile = 'reportgroup.json';
+$jsonData = file_get_contents($jsonFile);
+$reportGrpList = json_decode($jsonData, true);
+
+$jiraLink = 'https://trustarc.atlassian.net/';
+$awsLink = 'https://us-west-2.console.aws.amazon.com/';
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Sanitize and validate input data
+    $accountId = htmlspecialchars(trim($_POST["accountId"]));
     $key = htmlspecialchars(trim($_POST["key"]));
     $secret = htmlspecialchars(trim($_POST["secret"]));
     $token = htmlspecialchars(trim($_POST["token"]));
 
     // Basic validation
-    if (empty($key) || empty($secret) || empty($token)) {
+    if (empty($accountId) || empty($key) || empty($secret) || empty($token)) {
         echo "All fields are required.";
         exit;
-    } 
+    }
 
     $enableFetching = true;
-    
+
 } else {
     // If the form wasn't submitted, redirect or show an error
-    echo "NO CREDENTIALS SUPPLEMENTED";
+    echo "No Credentials Found. Cannot generate report.";
 }
-
-$reportGrpList = [
-    'ccm-ss-portal-api-regression-tests',
-    'ccm-ss-portal-ui-regression-tests',
-    'ccm-ss-advanced-cm-only-regression-tests',
-    'ccm-ss-standard-cm-only-regression-tests',
-    'ccm-reporting-api',
-    'ccm-ss-portal-api-smoke-qa-tests',
-    'ccm-ss-portal-ui-smoke-qa-tests',
-    'ccm-ss-advanced-cm-only-smoke-qa-tests',
-    'ccm-ss-standard-cm-only-smoke-qa-tests',
-    'ccm-legacy-two-step-optin-component',
-    'ccm-legacy-cms-component',
-    'ccm-legacy-icon-server-component',
-    'ccm-legacy-tdp-component',
-    'ccm-legacy-uat-parallel-tests',
-    'ccm-legacy-autoblock-component',
-    'ccm-legacy-widget-regression',
-    'ccm-legacy-dnt-component',
-    'ccm-legacy-iab-component',
-    'ccm-portal-ui-regression-tests-playwright',
-    'ccm-advanced-cm-regression-tests-playwright',
-    'ccm-portal-ui-smoke-tests-playwright'
-];
 
 ?>
 
@@ -72,6 +58,10 @@ $reportGrpList = [
         <h4>AWS Credentials</h4>
         <form action="#" method="POST">
             <div class="form-group">
+                <label for="key">ACCOUNT ID</label>
+                <input type="text" id="accountId" name="accountId" required autocomplete="off" value="<?php echo $accountId; ?>">
+            </div>
+            <div class="form-group">
                 <label for="key">KEY</label>
                 <input type="text" id="key" name="key" required autocomplete="off" value="<?php echo $key; ?>">
             </div>
@@ -88,8 +78,9 @@ $reportGrpList = [
     </div>
 
     <div class="report-container">
+        <button id="copyButton">Copy Table</button>
         <h4>Automation Report - <?php echo date('M d, Y'); ?></h4>
-        <table>
+        <table id="reportTable">
             <thead>
                 <td>Name</td>
                 <td>Test<br />Scenarios</td>
@@ -105,7 +96,7 @@ $reportGrpList = [
             </thead>
             <tbody>
                 <?php
-                if($enableFetching) {
+                if ($enableFetching) {
 
                     $client = new CodeBuildClient([
                         'version' => 'latest',
@@ -117,68 +108,81 @@ $reportGrpList = [
                         ],
                     ]);
 
+                    foreach ($reportGrpList as $key => $reportGrp) {
+                        $reportGroupArn = "arn:aws:codebuild:us-west-2:" . $accountId . ":report-group/" . $key . "-SurefireReports";
 
-                    foreach ($reportGrpList as $reportGrp) {
-
-                        $reportGroupArn = 'arn:aws:codebuild:us-west-2:654654316198:report-group/' . $reportGrp . '-SurefireReports';
-        
                         try {
-        
+
                             $result = $client->listReportsForReportGroup([
                                 'reportGroupArn' => $reportGroupArn,
                                 'sortBy' => 'CREATED_TIME',
                                 'maxResults' => 1,
                             ]);
-        
+
                             $reportArns = $result['reports'];
-        
+
                             foreach ($reportArns as $reportArn) {
-        
+
                                 // Get details of each report
                                 $reportDetails = $client->batchGetReports([
                                     'reportArns' => [$reportArn],
                                 ]);
-        
-                                foreach ($reportDetails['reports'] as $report) {
-                                    $showAll = false;
-                                    $created = date_format(date_create($report['created']), 'M d, Y H:i:s');
-        
-                                    if ($onlyScanForToday) {
-                                        if (date('Y-m-d', strtotime($report['created'])) == date('Y-m-d')) {
-                                            $showAll = true;
-                                        }
-                                    } else {
+
+                                $report = $reportDetails['reports'][0];
+
+                                $showAll = false;
+                                $created = date_format(date_create($report['created']), 'M d, Y H:i:s');
+
+                                if ($onlyScanForToday) {
+                                    if (date('Y-m-d', strtotime($report['created'])) == date('Y-m-d')) {
                                         $showAll = true;
                                     }
-        
-                                    if ($showAll) {
-                                        $reportArnName = explode("/", $report['arn'])[1];
-        
-                                        $testSummary = $report['testSummary']['statusCounts'];
-                                        $succeeded = $testSummary['SUCCEEDED'];
-                                        $failed = $testSummary['FAILED'];
-                                        $skipped = $testSummary['SKIPPED'];
-                                        $totalTests = $succeeded + $failed + $skipped;
-                                        $blocker = '';
-                                        $passedInLocalRun = '';
-                                        $bugFound = '';
-                                        $autoTicket = '';
-                                        $assignee = '';
-                                        $remarks = '';
-                                        echo '<tr>';
-                                        echo "<td><a href='https://us-west-2.console.aws.amazon.com/codesuite/codebuild/654654316198/projects/" . $reportGrp . "/history?region=us-west-2'>" . $reportGrp . "</a></td>"; // Name
-                                        echo "<td>" . $totalTests . "</td>"; // Test Scenarios
-                                        echo "<td><a target='_blank' href='https://us-west-2.console.aws.amazon.com/codesuite/codebuild/654654316198/testReports/reports/ccm-pr-roi-SurefireReports/" . $reportArnName . "?region=us-west-2'>" . $succeeded . "</a></td>"; // Passed
-                                        echo "<td>" . $failed . "</td>"; // Failed
-                                        echo "<td>" . $skipped . "</td>"; // Skipped
-                                        echo "<td>" . $blocker . "</td>"; // Blocker
-                                        echo "<td>" . $passedInLocalRun . "</td>"; // Passed in Local Run
-                                        echo "<td>" . $bugFound . "</td>"; // Bug Found
-                                        echo "<td>" . $autoTicket . "</td>"; // Bug Found
-                                        echo "<td>" . $assignee . "</td>"; // Bug Found
-                                        echo "<td>" . $remarks . "</td>"; // Bug Found 
-                                        echo '</tr>';
+                                } else {
+                                    $showAll = true;
+                                }
+
+                                if ($showAll) {
+                                    $reportArnName = explode("/", $report['arn'])[1];
+
+                                    $testSummary = $report['testSummary']['statusCounts'];
+                                    $succeeded = $testSummary['SUCCEEDED'];
+                                    $failed = $testSummary['FAILED'];
+                                    $skipped = $testSummary['SKIPPED'];
+                                    $totalTests = $succeeded + $failed + $skipped;
+                                    $blocker = '';
+                                    $passedInLocalRun = '';
+
+                                    $bugs = '';
+                                    $totalBugs = count($reportGrp[0]['bugs']);
+
+                                    foreach ($reportGrp[0]['bugs'] as $index=>$bugsList) {
+                                        if ($index === $totalBugs - 1) {
+                                            // Last item, do not add a comma
+                                            $bugs .= "<a href='" . $jiraLink . "browse/" . $bugsList . "' >" . $bugsList . "</a>";
+                                        } else {
+                                            // Not the last item, add a comma
+                                            $bugs .= "<a href='" . $jiraLink . "browse/" . $bugsList . "' >" . $bugsList . "</a>, ";
+                                        }
                                     }
+
+                                    $autoTicket = "<a href='" . $jiraLink . "browse/" . $reportGrp[0]['autoticket'] . "' >" . $reportGrp[0]['autoticket'] . "</a> ";
+                                    
+                                    $assignee = $reportGrp[0]['assignee'];
+                                    $remarks = $reportGrp[0]['remarks'];
+
+                                    echo '<tr>';
+                                    echo "<td><a href='" . $awsLink . "codesuite/codebuild/" . $accountId . "/projects/" . $key . "/history?region=us-west-2'>" . $key . "</a></td>"; // Name
+                                    echo "<td>" . $totalTests . "</td>"; // Test Scenarios
+                                    echo "<td><a target='_blank' href='" . $awsLink . "codesuite/codebuild/" . $accountId . "/testReports/reports/ccm-pr-roi-SurefireReports/" . $reportArnName . "?region=us-west-2'>" . $succeeded . "</a></td>"; // Passed
+                                    echo "<td>" . $failed . "</td>"; // Failed
+                                    echo "<td>" . $skipped . "</td>"; // Skipped
+                                    echo "<td>" . $blocker . "</td>"; // Blocker
+                                    echo "<td>" . $passedInLocalRun . "</td>"; // Passed in Local Run
+                                    echo "<td>" . $bugs . "</td>"; // Bug Found
+                                    echo "<td>" . $autoTicket . "</td>"; // Bug Found
+                                    echo "<td>" . $assignee . "</td>"; // Bug Found
+                                    echo "<td>" . $remarks . "</td>"; // Bug Found 
+                                    echo '</tr>';
                                 }
                             }
                         } catch (AwsException $e) {
@@ -201,11 +205,12 @@ $reportGrpList = [
     }
 
 
-        /* FORM STYLES */
+    /* FORM STYLES */
     .report-container {
         background: white;
         padding: 20px;
     }
+
     td:not(:first-child) {
         text-align: center;
     }
@@ -216,7 +221,7 @@ $reportGrpList = [
 
     table td {
         padding: 15px;
-        line-break: anywhere;
+        overflow: hidden;
     }
 
     table thead td {
@@ -231,6 +236,8 @@ $reportGrpList = [
         color: #000000;
         font-size: 13px;
         border: 1px solid #000000;
+        line-break: anywhere;
+        word-wrap: break-word;
     }
 
     table tbody tr {
@@ -250,20 +257,25 @@ $reportGrpList = [
     }
 
     .form-group {
-        display: flex; /* Use flexbox for inline layout */
-        justify-content: space-between; /* Distribute space between items */
-        align-items: center; /* Align items vertically */
+        display: flex;
+        /* Use flexbox for inline layout */
+        justify-content: space-between;
+        /* Distribute space between items */
+        align-items: center;
+        /* Align items vertically */
         margin-bottom: 15px;
     }
 
     label {
-        margin-right: 10px; /* Space between label and input */
-        flex: 0 0 20%; /* Allow label to take up a fixed width */
+        margin-right: 10px;
+        /* Space between label and input */
+        flex: 0 0 20%;
+        /* Allow label to take up a fixed width */
         font-size: 13px;
     }
 
     input[type="text"],
-    input[type="email"]{
+    input[type="email"] {
         width: 100%;
         padding: 10px;
         border: 1px solid #ccc;
@@ -292,6 +304,34 @@ $reportGrpList = [
     button:hover {
         background-color: #0056b3;
     }
+
+    button#copyButton {
+        width: 100px;
+        padding: 10px;
+    }
 </style>
+
+<script>
+    document.getElementById('copyButton').addEventListener('click', function() {
+        const table = document.getElementById('reportTable');
+        const range = document.createRange();
+        range.selectNode(table);
+        
+        const selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        
+        try {
+            const successful = document.execCommand('copy');
+            const msg = successful ? 'Table copied to clipboard!' : 'Failed to copy table.';
+            alert(msg);
+        } catch (err) {
+            console.error('Error copying table: ', err);
+        }
+        
+        // Clear the selection
+        selection.removeAllRanges();
+    });
+</script>
 
 </html>
